1aaf61473b8be5f17f68e37fefd05c50b4eb91a6
jdorta08 <dortajavier.jd2@gmail.com>
2023-02-16 16:27:52 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-red-300">
+      <h1>APP</h1>
+      <div className="w-10 h-10 bg-black-300">
+        <h2>DIV</h2>
+        <div>HOLA</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
