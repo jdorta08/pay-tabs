100736af77b0fbc4ea6c9a467edad3499756169f
jdorta08 <dortajavier.jd2@gmail.com>
2023-05-08 14:09:33 -0400

    chnges to style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -22,26 +22,18 @@ function Card({ status, setStatus, data }) {
   //END OF STYLES FOR STATUS TEXT
   return (
     <div className="w-full flex flex-col">
-      <div
-        key={data.id}
-        className="w-8/12 bg-gray-100 mx-7 my-2 drop-shadow-md p-8 -md:w-11/12 -md:mx-auto"
-      >
+      <div key={data.id} className="w-8/12 bg-gray-100 mx-7 my-2 drop-shadow-md p-8 -md:w-11/12 -md:mx-auto">
         <div className="flex flex-col">
           <h2 className="text-xl font-semibold">{data.title}</h2>
           <p className="text-base">{data.description}</p>
           <h3 className="font-medium">
             Status: <span style={styles}>{taskStatus}</span>
           </h3>
-          <div className="flex justify-start py-1 gap-2">
-            <button
-              className="bg-blue-600 text-white px-4 py-2 rounded-sm "
-              onClick={statusHandler}
-            >
+          <div className="flex justify-between pt-4">
+            <button className="bg-blue-600 text-white px-4 py-2 rounded-sm " onClick={statusHandler}>
               Mark as {buttonText}
             </button>
-            <button className="bg-gray-500 text-white px-4 py-2 rounded-sm">
-              Archive Tasks
-            </button>
+            <button className=" text-gray-500 px-4 py-2 rounded-sm">Archive Tasks</button>
           </div>
         </div>
       </div>
--- src/components/StatusCards.jsx
+++ src/components/StatusCards.jsx
@@ -3,14 +3,15 @@ import Card from "./Card";
 import cardData from "../data/cardData";
 
 function StatusCards() {
-  //map function iterates through the array cardData and returns a new array with an only property of completed set to false
+  //map function iterates through the array cardData and returns a new array
+  //with an only property of completed set to false
   const [status, setStatus] = useState(
     cardData.map(() => {
       return { completed: false };
     })
   );
-  //function that takes and index to know wich array item was clicked. Creates a copy of the status array using the spread operator and stores it in a newStatus var.
-  //
+  //function that takes and index to know wich array item was clicked.
+  //Creates a copy of the status array using the spread operator and stores it in a newStatus var.
   function statusHandler(index) {
     let newStatus = [...status];
     newStatus[index].completed = !newStatus[index].completed;
@@ -18,15 +19,10 @@ function StatusCards() {
   }
 
   return (
-    <div className="w-full flex flex-col">
+    <div className="flex flex-col">
       <h1 className="font-bold text-4xl -sm:text-3xl px-8 py-2">Dashboard</h1>
       {cardData.map((data, index) => (
-        <Card
-          key={data.id}
-          data={data}
-          status={status[index].completed}
-          setStatus={() => statusHandler(index)}
-        />
+        <Card key={data.id} data={data} status={status[index].completed} setStatus={() => statusHandler(index)} />
       ))}
     </div>
   );
